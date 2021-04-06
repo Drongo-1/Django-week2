9b3506cd4727148259730f28a0c1fdd0d48f6ff8
innovationnetworks-ke <ngugidavid46@gmail.com>
2021-04-06 21:51:01 +0300

    edited files

--- clone/email.py
+++ clone/email.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
     #Creating message subject and sender
     subject = 'Welcome to Instagram'
-    sender = 'donatello54611@gmail.com'
+    sender = 'ngugidavid46q@gmail.com'
 
     #passing in the context variables
     text_content = render_to_string('email/igemail.txt',{"name":name})
